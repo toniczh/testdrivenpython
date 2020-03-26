797c83fda09f9798ccc618ab6dfc6bf2a4f5d1f2
Tony <toniczh@gmail.com>
2020-03-26 19:07:08 -0400

    woorking cin

--- hacking.py
+++ hacking.py
@@ -1,8 +1,12 @@
 from lxml import etree
 from bs4 import BeautifulSoup
-import urllib
+import urllib.request
 cin_file = "C:\\temp\\cin\\36352102.xml"
-source_file = urllib.urlopen("file://"+cin_file).read()
+url_file = urllib.request.pathname2url(cin_file)
+# print(url_file)
+source_file = urllib.request.urlopen("file:"+url_file).read()
 bs = BeautifulSoup(source_file,"xml")
+# find the receiver
 receiver = bs.find('sh:Receiver')
-print(receiver)
+receiver_id= receiver.find("sh:Identifier").get_text()
+print(receiver_id) 
