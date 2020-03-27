8713113c5311f50565b37bc1edd0a7f75180c681
Tony <toniczh@gmail.com>
2020-03-27 13:01:41 -0400

    django project initialization

---
+++ superlists/django_local_test.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import unittest
+import time
+class NewVistorTest(unittest.TestCase):
+    def setUp(self):
+        chrome_path = "c:\\chromedriver\\chromedriver.exe"
+        self.driver = webdriver.Chrome(chrome_path)
+    def tearDown(self):
+        self.driver.quit()
+    def test_start_a_list_retrieve_it_later(self):
+        self.driver.get("http://localhost:8000")
+        time.sleep(3)
+        assert "Django" in self.driver.title
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
